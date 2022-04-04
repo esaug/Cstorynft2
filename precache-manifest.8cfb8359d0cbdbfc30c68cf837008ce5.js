fada3b0e9548842d56242e9a19506ebdbe10d544
Esau Guerra <46943313+esaug@users.noreply.github.com>
2022-04-04 07:07:38 -0400

    Add files via upload
    New version with the final contract

---
+++ precache-manifest.8cfb8359d0cbdbfc30c68cf837008ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001089c17ae1ff540d90b2d261bf18ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ca6d67e5e970eb46761",
+    "url": "/static/css/main.66dd6483.chunk.css"
+  },
+  {
+    "revision": "6899c26117eaa74e08f2",
+    "url": "/static/js/2.62756173.chunk.js"
+  },
+  {
+    "revision": "bea1eeb7636f643b8c79f74ea8b64c89",
+    "url": "/static/js/2.62756173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca6d67e5e970eb46761",
+    "url": "/static/js/main.e3879e18.chunk.js"
+  },
+  {
+    "revision": "dec26090cf87837f80e5",
+    "url": "/static/js/runtime-main.e95b8414.js"
+  }
+]);
